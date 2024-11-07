5a119dba1259e05731d68012638da7ed452130a0
Shabna Seethi <86661742+shabnaseethi@users.noreply.github.com>
2024-11-07 12:39:56 +0000

    Update JenkinsspringmvcApplication.java

--- src/main/java/com/shbz/jenkinsspringmvc/JenkinsspringmvcApplication.java
+++ src/main/java/com/shbz/jenkinsspringmvc/JenkinsspringmvcApplication.java
@@ -9,6 +9,7 @@ public class JenkinsspringmvcApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsspringmvcApplication.class, args);
 		System.out.println("Hello");
+		System.out.println("Jenkins Started");
 	}
 
 }
