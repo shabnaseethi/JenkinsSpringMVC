b4aa1ddb92f85855213a23ef39fedacd5d138b45
shabnaseethi <shabnaseethi@gmail.com>
2024-11-07 12:11:11 +0000

    Initial Commit

---
+++ src/main/java/com/shbz/jenkinsspringmvc/JenkinsspringmvcApplication.java
@@ -0,0 +1,14 @@
+package com.shbz.jenkinsspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsspringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsspringmvcApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
